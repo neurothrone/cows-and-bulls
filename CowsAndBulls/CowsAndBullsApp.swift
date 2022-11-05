f48df5bc57cb5b013280d7a84dcd1e14b687820d
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-11-05 03:53:45 +0100

    Initial Commit

---
+++ CowsAndBulls/CowsAndBullsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CowsAndBullsApp.swift
+//  CowsAndBulls
+//
+//  Created by Zaid Neurothrone on 2022-11-05.
+//
+
+import SwiftUI
+
+@main
+struct CowsAndBullsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
